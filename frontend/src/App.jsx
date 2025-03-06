188bfe7eb27b3bff626740e2da47c739918a4e70
bharat243 <142686411+bharat243@users.noreply.github.com>
2025-03-06 22:59:45 +0530

    add basic signup and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
+import SignupForm from "./components/ui/SignupForm.jsx";
+import LoginForm from "./components/ui/LoginForm.jsx";
+import CompanySignup from "./components/ui/companysignup.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OTPVerification from "./components/ui/Otpverify.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <>
+    
+
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<SignupForm />} /> 
+      <Route path='/register' element={<SignupForm/>}></Route>
+      <Route path='/login' element={<LoginForm/>}></Route>
+      <Route path='/companyregister' element={<CompanySignup/>}></Route>
+      <Route path='/otpverify' element={<OTPVerification/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    
+   </>
+
+  );
 }
 
-export default App
+export default App;
